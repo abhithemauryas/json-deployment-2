a8dbe739b4a6b50dfdfd0c9cd5a3ace56aa5a301
abhithemauryas <abhithemauryas5@gmail.com>
2023-09-14 01:13:59 +0530

    done

--- json-server/server.js
+++ json-server/server.js
@@ -1,13 +1,10 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server"); 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3001; 
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+server.listen(port);
